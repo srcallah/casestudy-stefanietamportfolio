47783b552391e3cc55d06612864f306836233a6d
Samantha <samantha.r.callahan@gmail.com>
2018-12-07 13:07:41 -0600

    Experiementing with Grids/Troubleshooting scaling/flex Please enter the commit message for your changes. Lines starting

--- js/main.js
+++ js/main.js
@@ -12,16 +12,62 @@ $(document).ready(function() {
      $("#name-hover").hide();
   });
 
-/* Titles on left in viewfinder
+/* Titles on left in viewfinder-------*/
+  $(".project-info-no, .project-title-lg").hover(function() {
+     $("#name-hover, #information-hover").hide();
+      });
 
-  $("#header-information").hover(function() {
-     $("#information-hover").show();
+  $("#nav-title-1").hover(function() {
+     $("#slide0-1, #title0-1, #acro1").show();
     },
   function(){
-        $("#information-hover").hide();
+        $("#slide0-1, #title0-1, #acro1").hide();
   });
 
-*/
+  $("#nav1-1").hover(function() {
+     $("#slide1-1, #image1-1, #acro1").show();
+    },
+  function(){
+        $("#slide1-1, #image1-1, #acro1").hide();
+  });
+
+  $("#nav2-1").hover(function() {
+     $("#slide2-1, #image2-1, #acro1").show();
+    },
+  function(){
+        $("#slide2-1, #image2-1, #acro1").hide();
+  });
+
+  $("#nav3-1").hover(function() {
+     $("#slide3-1, #image3-1, #acro1").show();
+    },
+  function(){
+        $("#slide3-1, #image3-1, #acro1").hide();
+  });
+
+  $("#nav4-1").hover(function() {
+     $("#slide4-1, #image4-1, #acro1").show();
+    },
+  function(){
+        $("#slide4-1, #image4-1, #acro1").hide();
+  });
+
+
+
+  $("#nav5-1").hover(function() {
+     $("#slide5-1, #image5-1, #acro1").show();
+    },
+  function(){
+        $("#slide5-1, #image5-1, #acro1").hide();
+  });
+
+  $("#navabc-1").hover(function() {
+     $("#slide6-abc1, #image6-1, #acro1").show();
+    },
+  function(){
+        $("#slide6-abc1, #image6-1, #acro1").hide();
+  });
+
 
 /*End javascripti---------------------------------*/
 });
