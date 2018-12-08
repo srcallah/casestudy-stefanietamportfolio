9f32a2e2ac83f6f00435a4a96726e45f6a303af7
Samantha <samantha.r.callahan@gmail.com>
2018-12-07 20:58:51 -0600

    Completed javascript for viewfinder

--- js/main.js
+++ js/main.js
@@ -13,6 +13,9 @@ $(document).ready(function() {
   });
 
 /* Titles on left in viewfinder-------*/
+
+/*Project 1*/
+
   $(".project-info-no, .project-title-lg").hover(function() {
      $("#name-hover, #information-hover").hide();
       });
@@ -52,8 +55,6 @@ $(document).ready(function() {
         $("#slide4-1, #image4-1, #acro1").hide();
   });
 
-
-
   $("#nav5-1").hover(function() {
      $("#slide5-1, #image5-1, #acro1").show();
     },
@@ -67,6 +68,133 @@ $(document).ready(function() {
   function(){
         $("#slide6-abc1, #image6-1, #acro1").hide();
   });
+
+  /*Project 2*/
+
+  $("#nav1-2").hover(function() {
+     $("#slide1-2, #image1-2, #acro2").show();
+    },
+  function(){
+        $("#slide1-2, #image1-2, #acro2").hide();
+  });
+
+  $("#nav2-2").hover(function() {
+     $("#slide2-2, #image2-2, #acro2").show();
+    },
+  function(){
+        $("#slide2-2, #image2-2, #acro2").hide();
+  });
+
+  $("#nav3-2").hover(function() {
+     $("#slide3-2, #image3-2, #acro2").show();
+    },
+  function(){
+        $("#slide3-2, #image3-2, #acro2").hide();
+  });
+
+  $("#nav4-2").hover(function() {
+     $("#slide4-2, #image4-2, #acro2").show();
+    },
+  function(){
+        $("#slide4-2, #image4-2, #acro2").hide();
+  });
+
+  $("#nav5-2").hover(function() {
+     $("#slide5-2, #image5-2, #acro2").show();
+    },
+  function(){
+        $("#slide5-2, #image5-2, #acro2").hide();
+  });
+
+  $("#navabc2").hover(function() {
+     $("#slide6-abc2, #image6-2, #acro2").show();
+    },
+  function(){
+        $("#slide6-abc2, #image6-2, #acro2").hide();
+  });
+
+  /*project 3*/
+
+  $("#nav1-3").hover(function() {
+     $("#slide1-3, #image1-3, #acro3").show();
+    },
+  function(){
+        $("#slide1-3, #image1-3, #acro3").hide();
+  });
+
+  $("#nav2-3").hover(function() {
+     $("#slide2-3, #image2-3, #acro3").show();
+    },
+  function(){
+        $("#slide2-3, #image2-3, #acro3").hide();
+  });
+
+  $("#nav3-3").hover(function() {
+     $("#slide3-3, #image3-3, #acro3").show();
+    },
+  function(){
+        $("#slide3-3, #image3-3, #acro3").hide();
+  });
+
+  $("#nav4-3").hover(function() {
+     $("#slide4-3, #image4-3, #acro3").show();
+    },
+  function(){
+        $("#slide4-3, #image4-3, #acro3").hide();
+  });
+
+  $("#nav5-3").hover(function() {
+     $("#slide5-3, #image5-3, #acro3").show();
+    },
+  function(){
+        $("#slide5-3, #image5-3, #acro3").hide();
+  });
+
+  $("#nav6-3").hover(function() {
+     $("#slide6-3, #image6-3, #acro3").show();
+    },
+  function(){
+        $("#slide6-3, #image6-3, #acro3").hide();
+  });
+
+  $("#navabc3").hover(function() {
+     $("#slide7-3, #image7-3, #acro3").show();
+    },
+  function(){
+        $("#slide7-abc3, #image7-3, #acro3").hide();
+  });
+
+  /*Project 4*/
+
+  $("#nav1-4").hover(function() {
+     $("#slide1-4, #image1-4, #acro4").show();
+    },
+  function(){
+        $("#slide1-4, #image1-4, #acro4").hide();
+  });
+
+  $("#nav2-4").hover(function() {
+     $("#slide2-4, #image2-4, #acro4").show();
+    },
+  function(){
+        $("#slide2-4, #image2-4, #acro4").hide();
+  });
+
+  $("#nav3-4").hover(function() {
+     $("#slide3-4, #image3-4, #acro4").show();
+    },
+  function(){
+        $("#slide3-4, #image3-4, #acro4").hide();
+  });
+
+  $("#navabc4").hover(function() {
+     $("#slide4-4, #image4-4, #acro4").show();
+    },
+  function(){
+        $("#slide4-4, #image4-4, #acro4").hide();
+  });
+
+
 
 
 /*End javascripti---------------------------------*/
